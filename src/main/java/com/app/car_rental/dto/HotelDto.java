87da984705536b3858f9f3f4d26c3d72a9077214
Virendra Patil <virendraspatil1811@gmail.com>
2024-02-22 17:39:04 +0530

    first commit

---
+++ src/main/java/com/app/car_rental/dto/HotelDto.java
@@ -0,0 +1,21 @@
+package com.app.car_rental.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HotelDto {
+
+    private String hotel;
+
+    private  String room;
+
+    private Integer stay;
+
+    private String manageParking;
+
+    private Integer finalCost;
+
+
+}
